22f98d42439850c824fbfd5826f50d700db61f1d
samuel_markus <markussamuel060@gmail.com>
2022-08-20 12:04:24 +0100

    test another date library

--- app.js
+++ app.js
@@ -6,36 +6,16 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
  var today = new Date();
- var currentDay = today.getDay();
- var day = "";
+ const options = { weekday: 'long', 
+ year: 'numeric',
+  month: 'long', 
+  day: 'numeric' }
 
- switch (currentDay ){
-    case 0:
-        day= "Sunday";
-        break;
-    case 1:
-        day= "Monday";
-        break;
-    case 2:
-        day= "Tuesday";
-        break;
-    case 3:
-        day= "Wednesday";
-        break;
-    case 4:
-        day= "Thursday";
-        break;
-    case 5:
-        day= "Friday";
-        break;
-    case 6:
-        day= "Saturday";
-        break;
-    
-    default:
-        console.log("error just occur when fatching day information")
+//  var currentDay =  today.getDay();
+ var day = today.toLocaleDateString("en-US", options);
 
- }
+ 
+
  res.render('list', {kindOfDay: day });
 
     
