d7608c9ce2e6da7e3471d047d8d4eb48e6c1c474
samuel_markus <markussamuel060@gmail.com>
2022-08-20 12:56:07 +0100

    todo list with array items

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+var items = ["Buy food", "Cook food","Eat food"];
 const app =express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended:true}))
 app.get("/", function(req, res){
  var today = new Date();
  const options = { weekday: 'long', 
@@ -11,17 +11,15 @@ app.get("/", function(req, res){
   month: 'long', 
   day: 'numeric' }
 
-//  var currentDay =  today.getDay();
  var day = today.toLocaleDateString("en-US", options);
-
- 
-
- res.render('list', {kindOfDay: day });
-
-    
+ res.render('list', {kindOfDay: day, newListItems: items});   
 });
 
-
+app.post("/", function(req, res){
+var item = req.body.newItem;
+items.push(item);
+res.redirect("/"); 
+});
 app.listen(3000, function(){
     console.log("Server sterting on port 3000");
 });
