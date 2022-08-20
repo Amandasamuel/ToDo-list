5c5b45ea7e275e790e065962af257ddd4b8abbb9
samuel_markus <markussamuel060@gmail.com>
2022-08-20 13:10:07 +0100

    just add css file to my project

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var items = ["Buy food", "Cook food","Eat food"];
+let items = ["Buy food", "Cook food","Eat food"];
 const app =express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.get("/", function(req, res){
- var today = new Date();
+ let today = new Date();
  const options = { weekday: 'long', 
  year: 'numeric',
   month: 'long', 
   day: 'numeric' }
 
- var day = today.toLocaleDateString("en-US", options);
+ let day = today.toLocaleDateString("en-US", options);
  res.render('list', {kindOfDay: day, newListItems: items});   
 });
 
 app.post("/", function(req, res){
-var item = req.body.newItem;
+let item = req.body.newItem;
 items.push(item);
 res.redirect("/"); 
 });
